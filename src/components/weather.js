2122028f749f0635d455a76375cff20de0acdedd
lauren-richardson <lauren.richardson13@gmail.com>
2022-02-14 13:43:53 -0500

    name to appear

--- src/components/weather.js
+++ src/components/weather.js
@@ -6,19 +6,17 @@ function CurrentWeather () {
     const [currentWeather, setCurrentWeather] = useState([])
 
     useEffect(() => {
-        fetch("http://api.openweathermap.org/data/2.5/weather?lat=42.3314&lon=-83.0458&appid=67bb6623a4284a1abf2dcd12ee9ea8d6&units=imperial")
+        fetch("http://api.openweathermap.org/data/2.5/weather?lat=42.3314&lon=-83.0458&appid=7f6ac03571b9827d5308f728f9548ca4&units=imperial")
         .then(res => res.json())
             .then(
                 (result) => {
                     setCurrentWeather(result)
                 })})
-    console.log(currentWeather)
-
     // console.log(currentWeather)
+
+    console.log(currentWeather)
     return (
-        <section>
-            <div>{currentWeather}</div>
-        </section>
+        <div>{currentWeather.name}</div>
     )
     
 }
